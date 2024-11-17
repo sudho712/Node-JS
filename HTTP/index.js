80c23837d4f997b488dc8034ce041ef8e3095884
Sudhir Kumar <sudho712@gmail.com>
2024-11-17 22:43:30 +0530

    show user search details like hoe date show log.txt file

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,9 +1,21 @@
-const { Console } = require("console");
-const http=require("http");
 
-const myServer=http.createServer((req, res)=>{
-    console.log("new Req Rec.");
+const http = require("http");
+const fs=require("fs");
+
+const myServer = http.createServer((req, res) => {
+    const log=`${Date.now()}: new req Received\n`;
+    fs.appendFile("log.txt",log,(err,data) => {
+
+        console.log("New Request Received.");
+
     console.log("Hello from server");
 
+    // Responding to the client
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello from the server! br............");
+    });
+
+    
 });
-myServer.listen(3000,() => console.log("Server started"));
+
+myServer.listen(3000, () => console.log("Server started on port 3000"));
--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const http = require("http");
 const fs=require("fs");
 
 const myServer = http.createServer((req, res) => {
-    
-    console.log("New Request Received.");
+    const log=`${Date.now()}: ${req.url} new req Received\n`;
+    fs.appendFile("log.txt",log,(err,data) => {
+
+        console.log("New Request Received.");
 
     console.log("Hello from server");
 
     // Responding to the client
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello from the server! br............");
+    });
+
+    
 });
 
 myServer.listen(3000, () => console.log("Server started on port 3000"));
