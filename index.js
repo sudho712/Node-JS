7d90a3e794771514298f22f6d01e9b6fed3863ac
Sudhir Kumar <sudho712@gmail.com>
2024-11-17 23:17:32 +0530

    http day end

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const myServer = http.createServer((req, res) => {
     const log=`${Date.now()}: ${req.url} new req Received\n`;
     fs.appendFile("log.txt",log,(err,data) => {
 
+        switch(req.url)
+        {
+            case '/': res.end("Homepage");
+            break
+            case '/about' : res.end("Hello node js");
+            default:
+                res.end("404 not found")
+        }
+
         console.log("New Request Received.");
 
     console.log("Hello from server");
