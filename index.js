88bf01071191fdfffeac218a6accc853878c6391
Sudhir Kumar <sudho712@gmail.com>
2024-11-23 19:03:06 +0530

    node js

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-
 const http = require("http");
-const fs=require("fs");
+const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-    const log=`${Date.now()}: ${req.url} new req Received\n`;
-    fs.appendFile("log.txt",log,(err,data) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.url} new req Received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Homepage");
+        break;
+      case "/about":
+        res.end("Hello node js");
+      default:
+        res.end("404 not found");
+    }
 
-        switch(req.url)
-        {
-            case '/': res.end("Homepage");
-            break
-            case '/about' : res.end("Hello node js");
-            default:
-                res.end("404 not found")
-        }
-
-        console.log("New Request Received.");
+    console.log("New Request Received.");
 
     console.log("Hello from server");
 
     // Responding to the client
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello from the server! br............");
-    });
-
-    
+  });
 });
 
 myServer.listen(3000, () => console.log("Server started on port 3000"));
